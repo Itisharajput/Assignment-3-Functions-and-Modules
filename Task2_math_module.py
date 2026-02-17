e8c602463e5d6b998743dcafbd297314baca76ea
Itisha Chouhan <itisharajput00@gmail.com>
2026-02-17 23:09:51 +0530

    Add math module based calculations program
    Created a Python program that utilizes the built-in math module to perform 
    mathematical calculations. The program takes user input and computes the square 
    root, natural logarithm (base e), and sine of the given number. This task highlights 
    the practical use of Python modules and mathematical functions with user input handling.

---
+++ Task2_math_module.py
@@ -0,0 +1,7 @@
+import math
+
+number1 = int(input("Enter a number:"))
+number = abs(number1)
+print(f"Square root of {number} is {math.sqrt(number)}")
+print(f"log  of {number} is {math.log(number)}")
+print(f"Sin of {number} is {math.sin(number)}")
