f47f22c25cf6bbd360270081211cdcb20647a82e
Itisha Chouhan <itisharajput00@gmail.com>
2026-02-25 10:27:49 +0530

    Fix factorial base case and add input validation
    Update the factorial function to correctly handle 0 as a base case and added input validation using try-except to handle invalid and negative  input gracefully.

--- Task1._factorial.py
+++ Task1._factorial.py
@@ -1,13 +1,19 @@
-number = int(input("Enter a number:"))
-num = abs(number)
 def factorial(num):
-"""
+       """
 Calculate factorial of a non-negative integer using recursion
-"""
-    if num == 1 or n == 0 :
-        return 1
+ """
+       if num == 1 or num == 0 :
+           return 1
+       else:
+    # Calling the function using recursion
+           num_factorial = num * factorial(num-1)
+       return num_factorial
+try :
+    num = int(input("Enter a number :"))
+    if num < 0:
+              print("Factorial is not defined for negative numbers.")
     else:
-        num_factorial = num * factorial(num-1)
-        return num_factorial
-    
-print(f" Factorial of {num} is {factorial(num)}")
+              print(f"Factorial of {num} is {factorial(num)}")
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+   
