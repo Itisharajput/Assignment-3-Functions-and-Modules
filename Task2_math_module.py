33b68db6843ce7d79123ecb0af143be823f0e73e
Itisha Chouhan <itisharajput00@gmail.com>
2026-02-25 10:52:32 +0530

    Fix sine calculation by converting degrees to radians
    Updated sine calculation to convert degree input into radians and added comments explaining the mathematical reasoning  behind each operation.

--- Task2_math_module.py
+++ Task2_math_module.py
@@ -1,7 +1,21 @@
+    
 import math
 
-number1 = int(input("Enter a number:"))
-number = abs(number1)
-print(f"Square root of {number} is {math.sqrt(number)}")
-print(f"log  of {number} is {math.log(number)}")
-print(f"Sin of {number} is {math.sin(number)}")
+try:
+# Take integer input from the user
+    number = int(input("Enter a number :"))
+# Square root is calculated using absolute value to avoid errors
+    abs_number = abs(number)
+    print(f"Square root of {abs_number} is {math.sqrt(abs_number)}")
+
+# Logarithm is defined only for positive numbers
+    if number > 0:
+        print(f"Log of {number} is {math.log(number)}")
+    else:
+        print("Logarithm is not defined for zero or negative numbers.")
+# Sine calculation (works for all real numbers)
+    radians = math.radians(number)
+    print(f"Sin of {number} is {math.sin(radians)}")
+except ValueError:
+# Handling non-integer input
+    print("Invalid input. Please enter an integer.")
